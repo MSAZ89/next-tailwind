dddb5f4caab2c726a52bbb3cc717e698bd76c4f4
MSAZ89 <michaelsimonaz@gmail.com>
2022-03-19 11:27:58 -0700

    Updated CSS

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Home() {
 
   return (
     <div className="container mx-auto">
-      <button className="bg-orange-600 px-4 py-1 border-2 border-black-400" onClick={ () => updateData(document.getElementById("data").value)}>Update Data</button>
+      <button className="bg-green-600 px-4 py-1 border-2 border-black-400" onClick={ () => updateData(document.getElementById("data").value)}>Update Data</button>
       <br/>
       <input className="bg-gray-400" id="data" type="text" />
       <br/><br/>
-      <p>{data}</p>
+      <h2>{data}</h2>
 
     </div>
   )
